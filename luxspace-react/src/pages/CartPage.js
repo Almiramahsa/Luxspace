99e03f91e92b3eb8b45ccc8dc8143d8c783c0edf
Almira Mahsa <almiramahsa9@gmail.com>
2023-01-24 14:03:05 +0700

    editing cart page (add shopping & shipping part)

--- luxspace-react/src/pages/CartPage.js
+++ luxspace-react/src/pages/CartPage.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Header from 'parts/Header';
 import Footer from 'parts/Footer';
-import Breadcrum from 'components/Breadcrum';
+import Breadcrumb from 'components/Breadcrumb';
+import ShoppingCart from './Cart/ShoppingCart';
+import ShippingDetails from './Cart/ShippingDetails';
 
 export default function CartPage() {
   return (
     <>
       <Header theme="black" />
-      <Breadcrum
+      <Breadcrumb
         list={[
           { url: '/', name: 'Home' },
-          { url: '/categories/921', name: 'Office Room' },
-          { url: '/categories/921/products/212', name: 'Details' },
+          { url: '/cart', name: 'Shopping Cart' },
         ]}
       />
+      <section className="md:py-16">
+        <div className="container mx-auto px-4">
+          <div className="flex -mx-4 flex-wrap">
+            <ShoppingCart />
+            <ShippingDetails />
+          </div>
+        </div>
+      </section>
 
       <Footer />
     </>
