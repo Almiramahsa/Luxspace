54719e2a7663d06a100b330e0d7592fee397ec6e
Almira Mahsa <almiramahsa9@gmail.com>
2023-01-25 07:40:23 +0700

    finish slicing part 1

--- luxspace-react/src/pages/HomePage.js
+++ luxspace-react/src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from 'parts/Header';
-import Hero from 'parts/Hero';
+import Hero from 'parts/Homepage/Hero';
 import JustArrived from 'parts/Homepage/JustArrived';
 import BrowseRoom from 'parts/Homepage/BrowseRoom';
 import Footer from 'parts/Footer';
