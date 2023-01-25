875ee97a9c79addbf487d095ea128e0b9c428cae
Almira Mahsa <almiramahsa9@gmail.com>
2023-01-25 14:44:50 +0700

    add new .env file, useAsync, useSafeDispatch and implement fetching api on browseroom page

---
+++ luxspace-react/src/helpers/fetch/index.js
@@ -0,0 +1,12 @@
+export default async function fetchData({ url, method = 'GET', host = process.env.REACT_APP_API_HOST, body }) {
+  const response = await fetch(`${host}${url}`, {
+    method,
+    mode: 'cors',
+    headers: { 'Content-Type': 'application/json' },
+    body,
+  });
+  const statusHasResponse = [200, 404];
+  const jsonResponse = statusHasResponse.includes(response.status) ? await response.json() : response;
+  if (response.ok) return jsonResponse;
+  throw new Error(JSON.stringify(jsonResponse));
+}
